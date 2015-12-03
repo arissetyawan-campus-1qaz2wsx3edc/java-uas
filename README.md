4ee0ea54c864dc8ced515ae6b8058d4f0b2207af
arissetyawan <arissetyawan.email@gmail.com>
2015-12-03 12:59:42 +0700

    r1

--- README.md
+++ README.md
@@ -1,1 +1,73 @@
-# java-uas
+# DATABASE
+Table yg harus dibuat: Plural/jamak
+-----------------------------
+1) Products (data barang)
+id autoincrement integer
+code varchar
+name
+price integer
+stock
+created_at (untuk menyimpan tgl ketika record ini dibuat)
+updated_at (untuk diupdate dgn taggl record di update)
+-----------------------------
+2) Users (untuk login):
+id
+username (untuk login)
+password (untuk password) 
+full_name
+phone
+created_at
+updated_at
+-----------------------------
+3) Orders (master transaksi)
+id
+total
+created_at
+updated_at
+-----------------------------
+4) Transactions (detail transaksi):
+order_id
+id
+product_id
+price
+quantity
+sub_total
+-----------------------------
+>> posuas.sql
+
+# UI MOCKUP/DESIGN:
+>> sbg referensi untuk mockup: mockup.zip
+
+# FLOW
+>> flow.zip
+
+# TODO
+1. phpmyadmin -> mysql (database), apache (webserver), php (lang) atau sejenisnya agar mempermudah generate /create table dst
+2. java
+3. netbeans
+cara install google: how to install phpmyadmin step by step, etc.
+
+# WORK
+1. buat database dan table2nya dulu via apapun blh, mau pma, atau workbench, etc
+2. buat GUInya (menu, main form, dan internal form)
+3. mulai koding
+   1. buat kelas dan method koneksi
+   2. buat method untuk exekusi query mysql
+   3. buat action performed dgn klik pada object2 GUI
+4. yg sudah selesai minta dinilai kapanpun dan selanjutnya tidak usah masuk kelas is OK.
+
+
+# classes for you :)
+class2 berikut ini untuk membantu anda untuk:
+
+1) mengetahui waktu sekarang yg akan dipake di created_at dan atau updated_at
+2) melakukan koneksi ke database
+3) mengexecute query SQL
+>> classes.zip
+
+
+Selamat belajar. ^_^
+
+arissan
+
+
---
+++ posuas.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Dec 03, 2015 at 12:47 PM
+-- Server version: 5.5.44-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `posuas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `total` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transactions`
+--
+
+CREATE TABLE IF NOT EXISTS `transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sub_total` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
